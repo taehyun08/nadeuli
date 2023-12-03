42cf3f4e28a987fd41e3557cdb2858539bfc24cf
Coalman96 <mayuaa001@gmail.com>
2023-12-04 05:35:54 +0900

    Fix : jenkins pipeline 수정
    테스트로 MemberApplication에 글자 추가

--- backend/src/test/java/kr/nadeuli/member/MemberApplicationTests.java
+++ backend/src/test/java/kr/nadeuli/member/MemberApplicationTests.java
@@ -267,7 +267,7 @@ public class MemberApplicationTests {
     ProductDTO productDTO = productService.getProduct(5L);
     ReportDTO reportDTO = ReportDTO.builder()
         .reportId(1L)
-        .content("걍 개띠꺼움")
+        .content("걍 개띠꺼움d")
         .reporter(memberDTO)
         .product(productDTO)
         .build();
