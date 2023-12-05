848c86d5b6deff30302df40af6876f2dc1e1cb4a
taehyun08 <didls1643@naver.com>
2023-12-05 09:38:22 +0900

    Fix : 상품 리스트 조회 버그수정

--- backend/src/main/java/kr/nadeuli/service/product/ProductRepository.java
+++ backend/src/main/java/kr/nadeuli/service/product/ProductRepository.java
@@ -5,9 +5,16 @@ import kr.nadeuli.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+    @Query("SELECT p FROM Product p WHERE p.title LIKE %:title% OR p.content LIKE %:content% AND p.gu = :gu")
+    Page<Product> findProductListByKeyword(@Param("title") String title, @Param("content") String content, @Param("gu") String gu, Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE p.gu = :gu")
+    Page<Product> findProductList(@Param("gu") String gu, Pageable pageable);
+
     Page<Product> findBySellerAndIsSold(Member seller, boolean isSold, Pageable pageable);
     Page<Product> findByBuyer(Member buyer, Pageable pageable);
 }
