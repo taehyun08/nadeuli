f36884a190d7fa7d4af5e615013b7d36f7cd4515
taehyun08 <didls1643@naver.com>
2023-12-06 17:22:24 +0900

    Feat : 채팅 서버 구현
    monggo디비 연결 관련 코드 채팅방 목록과 채팅을 조회하는 기능과 socket io로 실시간 채팅을 할 수 있게 구현함

--- chatserver/app.js
+++ chatserver/app.js
@@ -1,62 +1,147 @@
 const express = require('express');
+// const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const http = require('http');
-const mysql = require('mysql');
-const dbconfig = require('./config/db_config');
-const socketIo = require('socket.io');
+const socketIO = require('socket.io');
+// const mongoose = require('./dbConnector');
+const ChatRoom = require('./model/chatRoom');
+const ChatMessage = require('./model/chatMessage');
+const chatRoomRouter = require('./routes/chatRoom');
 
-const connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : 'qkwlek12!',
-  database : 'miniproject'
-});
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIO(server);
+const PORT = 3001;
 
-connection.connect((err)=>{
-  if(err){
-    console.error(err.stack);
-    return;
-  }
-  console.log(connection.threadId);
-});
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/chatRoom', chatRoomRouter);
 
 
-// 유저들의 소켓을 저장할 맵
-const users = new Map();
 
+
+// Socket.io 연결 설정
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('A user connected');
 
-  socket.on('private message', ({ sender, receiver, message }) => {
-    const receiverSocketId = users.get(receiver);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit('private message', {
-        sender: sender,
-        message,
-      });
+
+
+  // 채팅방 목록 조회
+  const updateChatRooms = async (tag) => {
+    try {
+      // 멤버가 참여한 채팅방 목록 조회
+      const chatRooms = await ChatRoom.find({ 'participants.tag': tag });
+      io.to(tag).emit('chatRooms', chatRooms);
+    } catch (error) {
+      console.error('Error fetching chat rooms:', error);
+    }
+  };
+
+  // 채팅방 목록 초기화
+  updateChatRooms();
+
+  // 멤버가 채팅방에 참가
+  socket.on('joinRoom', async (data) => {
+    const { tag, roomId } = data;
+    try {
+      const chatRoom = await ChatRoom.findById(roomId);
+
+      if (chatRoom) {
+        // 이미 참가 중인지 확인
+        const existingParticipant = chatRoom.participants.find(participant => participant.tag === tag);
+        if (!existingParticipant) {
+          // 참가하지 않았다면 참가
+          chatRoom.participants.push({
+            tag,
+            name: data.name,
+            joinTime: Date.now(),
+          });
+          await chatRoom.save();
+
+          // 채팅방 메시지 조회
+          const messages = await ChatMessage.find({ room: roomId }).populate('sender');
+          socket.emit('chatMessages', messages);
+        }
+      }
+    } catch (error) {
+      console.error('Error during joining room:', error);
     }
   });
 
-  socket.on('join-room', (roomId) => {
-    socket.join(roomId);
-    console.log(roomId, "에 참가하였습니다.");
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-    // 유저 제거
-    users.forEach((value, key) => {
-      if (value === socket.id) {
-        connection.end();
-        users.delete(key);
-      }
+  socket.on('getChatrooms', ({ userId }) => {
+    // 사용자가 참여한 채팅방 목록 조회
+    const chatrooms = getChatroomsForUser(userId);
+  
+    // 클라이언트에 채팅방 목록 전송
+    socket.emit('chatrooms', chatrooms);
+  
+    // 각 채팅방에 대해 소켓 연결 설정
+    chatrooms.forEach((room) => {
+      socket.join(room.id);
     });
   });
+
+  // 멤버가 채팅방에서 나가기
+  socket.on('leaveRoom', async (data) => {
+    const { tag, roomId } = data;
+    try {
+      const chatRoom = await ChatRoom.findById(roomId);
+
+      if (chatRoom) {
+        // 참가자 목록에서 제거
+        chatRoom.participants = chatRoom.participants.filter(participant => participant.tag !== tag);
+        await chatRoom.save();
+
+        // 채팅방 메시지 조회
+        const messages = await ChatMessage.find({ room: roomId }).populate('sender');
+        socket.emit('chatMessages', messages);
+
+        // 채팅방 목록 갱신
+        // updateChatRooms(tag);
+      }
+    } catch (error) {
+      console.error('Error during leaving room:', error);
+    }
+  });
+
+  // 채팅 메시지 전송
+  socket.on('sendMessage', async (data) => {
+    const { tag, roomId, message } = data;
+    try {
+      const chatRoom = await ChatRoom.findById(roomId);
+  
+      if (chatRoom) {
+        const participant = chatRoom.participants.find(participant => participant.tag === tag);
+  
+        if (participant) {
+          // 채팅 메시지 저장
+          const chatMessage = new ChatMessage({
+            sender: participant,
+            message,
+            room: roomId,
+          });
+          await chatMessage.save();
+  
+          // 채팅방 메시지 조회 (참가 시간 이후의 채팅만)
+          const joinTime = participant.joinTime;
+          const messages = await ChatMessage.find({ room: roomId, createdAt: { $gte: joinTime } }).populate('sender');
+          io.to(roomId).emit('chatMessages', messages);
+        }
+      }
+    } catch (error) {
+      console.error('Error during sending message:', error);
+    }
+  });
+
+  // 연결 해제 시
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
 
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
